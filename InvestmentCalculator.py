25adf007e81e74994b2f7411d4e423aa7ed980d4
Shawn Bishop <39964474+shawnpbishop@users.noreply.github.com>
2018-12-06 14:15:05 -0600

    Add files via upload

---
+++ InvestmentCalculator.py
@@ -0,0 +1,45 @@
+#This program is written by Shawn Bishop.
+
+from tkinter import * 
+    
+class InvestmentCalculator:
+        def __init__(self):
+            window = Tk() 
+            window.title("Investment Calculator") 
+            
+            Label(window, text = "Investment Amount").grid(row = 1, 
+                column = 1, sticky = W)
+            Label(window, text = "Years").grid(row = 2, 
+                column = 1, sticky = W)
+            Label(window, text = "Annual Interest Rate").grid(row = 3, 
+                column = 1, sticky = W)
+            Label(window, text = "Future Value").grid(row = 4, 
+                column = 1, sticky = W)
+            
+            self.investmentAmount = StringVar()
+            Entry(window, textvariable = self.investmentAmount, 
+                justify = RIGHT).grid(row = 1, column = 2)
+            self.annualInterestRate = StringVar()
+            Entry(window, textvariable = self.annualInterestRate, 
+                justify = RIGHT).grid(row = 3, column = 2)
+            self.numberOfYears = StringVar()
+            Entry(window, textvariable = self.numberOfYears, 
+                justify = RIGHT).grid(row = 2, column = 2)
+            self.futureValue = StringVar()
+            Label(window, textvariable = 
+                self.futureValue).grid(row = 4, column = 2, sticky = E)
+                
+            Button(window, text = "Calculate", 
+                command = self.computeFutureValue).grid(row = 6, column = 2, sticky = E)
+            
+            window.mainloop()
+        
+        def computeFutureValue(self):
+            monthlyInterestRate = float(self.annualInterestRate.get()) / 1200
+            futureValue = float(self.investmentAmount.get()) * \
+                (1 + monthlyInterestRate) ** (float(self.numberOfYears.get()) * 12)
+            self.futureValue.set("{0:10.2f}".format(futureValue))
+
+ 
+
+InvestmentCalculator()
